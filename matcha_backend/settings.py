ef59e41415345dcec78b1c923d8baeb3971d1c08
Akshit Sharma <gentleakshit@gmail.com>
2026-06-30 13:20:46 +0530

    chore: update default from email to gentleakshit@gmail.com

--- matcha_backend/settings.py
+++ matcha_backend/settings.py
@@ -159,9 +159,10 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = 'recruitment@matcha.ai'
+DEFAULT_FROM_EMAIL = 'gentleakshit@gmail.com'
 
 # Media Settings
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
